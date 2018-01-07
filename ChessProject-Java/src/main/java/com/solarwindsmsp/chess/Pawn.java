a7f56831d3fe3b6f0e71d39f025acf0f22c4bfc3
LEANIT\alagamma <alagamma@leanitconsulting.ch>
2018-01-07 17:44:56 +0100

    Implement Pawn MOVE method fixing existing tests

--- ChessProject-Java/src/main/java/com/solarwindsmsp/chess/Pawn.java
+++ ChessProject-Java/src/main/java/com/solarwindsmsp/chess/Pawn.java
@@ -2,58 +2,74 @@ package com.solarwindsmsp.chess;
 
 public class Pawn {
 
-    private ChessBoard chessBoard;
-    private int xCoordinate;
-    private int yCoordinate;
-    private PieceColor pieceColor;
+	private ChessBoard chessBoard;
+	private int xCoordinate;
+	private int yCoordinate;
+	private PieceColor pieceColor;
 
-    public Pawn(PieceColor pieceColor) {
-        this.pieceColor = pieceColor;
-    }
+	public Pawn(PieceColor pieceColor) {
+		setPieceColor(pieceColor);
+	}
 
-    public ChessBoard getChesssBoard() {
-        return chessBoard;
-    }
+	public ChessBoard getChesssBoard() {
+		return chessBoard;
+	}
 
-    public void setChessBoard(ChessBoard chessBoard) {
-        this.chessBoard = chessBoard;
-    }
+	public void setChessBoard(ChessBoard chessBoard) {
+		this.chessBoard = chessBoard;
+	}
 
-    public int getXCoordinate() {
-        return xCoordinate;
-    }
+	public int getXCoordinate() {
+		return xCoordinate;
+	}
 
-    public void setXCoordinate(int value) {
-        this.xCoordinate = value;
-    }
+	public void setXCoordinate(int value) {
+		this.xCoordinate = value;
+	}
 
-    public int getYCoordinate() {
-        return yCoordinate;
-    }
+	public int getYCoordinate() {
+		return yCoordinate;
+	}
 
-    public void setYCoordinate(int value) {
-        this.yCoordinate = value;
-    }
+	public void setYCoordinate(int value) {
+		this.yCoordinate = value;
+	}
 
-    public PieceColor getPieceColor() {
-        return this.pieceColor;
-    }
+	public PieceColor getPieceColor() {
+		return this.pieceColor;
+	}
 
-    private void setPieceColor(PieceColor value) {
-        pieceColor = value;
-    }
+	private void setPieceColor(PieceColor value) {
+		pieceColor = value;
+	}
 
-    public void Move(MovementType movementType, int newX, int newY) {
-        throw new UnsupportedOperationException("Need to implement Pawn.Move()") ;
-    }
+	public void Move(MovementType movementType, int newX, int newY) {
+		if (movementType == MovementType.CAPTURE) {
+			throw new UnsupportedOperationException("MovementType.CAPTURE is not implemented yet");
+		} else {
+			if (chessBoard.IsLegalMove(newX, newY) && this.IsLegalMove(newX, newY)) {
+				this.setXCoordinate(newX);
+				this.setYCoordinate(newY);
+			}
+		}
+	}
 
-    @Override
-    public String toString() {
-        return CurrentPositionAsString();
-    }
+	private boolean IsLegalMove(int newX, int newY) {
+		if(getPieceColor().equals(PieceColor.BLACK)) {
+			return newX == getXCoordinate() && newY  == getYCoordinate() - 1;
+		} else{
+			return newX == getXCoordinate() && newY  == getYCoordinate() + 1;
+		} 
+	}
 
-    protected String CurrentPositionAsString() {
-        String eol = System.lineSeparator();
-        return String.format("Current X: {1}{0}Current Y: {2}{0}Piece Color: {3}", eol, xCoordinate, yCoordinate, pieceColor);
-    }
+	@Override
+	public String toString() {
+		return CurrentPositionAsString();
+	}
+
+	protected String CurrentPositionAsString() {
+		String eol = System.lineSeparator();
+		return String.format("Current X: {1}{0}Current Y: {2}{0}Piece Color: {3}", eol, xCoordinate, yCoordinate,
+				pieceColor);
+	}
 }
--- ChessProject-Java/src/test/java/com/solarwindsmsp/chess/PawnTest.java
+++ ChessProject-Java/src/test/java/com/solarwindsmsp/chess/PawnTest.java
@@ -1,6 +1,5 @@
 package com.solarwindsmsp.chess;
 
-import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
